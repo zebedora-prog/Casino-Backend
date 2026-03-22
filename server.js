a606ef818d16f3d1c925c92c4ca6a1bcb3963922
zebedora-prog <zebedora@gmail.com>
2026-03-22 07:23:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
+https://your-railway-url/index.html
 
 // User schema
 const User = mongoose.model("User", {
