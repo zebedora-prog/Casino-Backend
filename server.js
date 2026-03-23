f9bf215444c27975120bf080ab576a193f28bf97
zebedora-prog <zebedora@gmail.com>
2026-03-23 14:28:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-app.use(cors());
+const path = require("path");
+
 const app = express();
-
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 app.use(express.json());
 
 // 👤 User Schema
