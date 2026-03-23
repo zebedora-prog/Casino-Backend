aa2f86b4691f4952f7b6f12536967d711393b2a2
zebedora-prog <zebedora@gmail.com>
2026-03-23 13:55:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
+app.use(cors());
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 // 👤 User Schema
